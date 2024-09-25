732a596e94ec665b7897d3c833466b506867303b
OlyaSilaeva-dev <olya-dom2014@yandex.ru>
2024-09-25 23:06:53 +0300

    New style

--- lab1-Progonka/Progonka.h
+++ lab1-Progonka/Progonka.h
@@ -1,3 +1,10 @@
+//
+// Created by olya- on 25.09.2024.
+//
+
+#ifndef CM_PROGONKA_H
+#define CM_PROGONKA_H
+
 #include <iostream>
 #include <vector>
 #include <filesystem>
@@ -161,33 +168,4 @@ void input_all(string const & file_name, vector<vector<double>> &input_matrix, v
     input_result = result;
 }
 
-int main(int argc, char * argv[]) {
-
-//    filesystem::current_path("/mnt/c/src/GitHub/CM");
-    filesystem::current_path("C:/src/GitHub/Numerical-methods");
-//    std::string file_name(argv[1]);
-    std::string file_name("test.txt");
-
-    vector<vector<double>> matrix;
-    vector<double> result;
-    input_all(file_name, matrix, result);
-
-    vector<double> result_operation;
-    result_operation = Method_Progonki(matrix, result);
-
-    size_t n = result_operation.size();
-
-    std::cout << "\nResult: " << std::endl;
-    for (int i = 0; i < n; i++) {
-        cout.precision(6);
-        cout << fixed << result_operation[i] << " ";
-    }
-
-    double det = determinant(matrix, matrix.size());
-    std::cout << "\n\nDeterminant: " << std::endl;
-
-    cout.precision(6);
-    cout << det << endl;
-
-    return 0;
-}
+#endif //CM_PROGONKA_H
---
+++ lab1-Progonka/test.cpp
@@ -0,0 +1,32 @@
+#include "Progonka.h"
+
+int main(int argc, char * argv[]) {
+
+//    filesystem::current_path("/mnt/c/src/GitHub/Numerical-methods/lab1-Progonka");
+    filesystem::current_path("C:/src/GitHub/Numerical-methods/lab1-Progonka");
+//    std::string file_name(argv[1]);
+    std::string file_name("test.txt");
+
+    vector<vector<double>> matrix;
+    vector<double> result;
+    input_all(file_name, matrix, result);
+
+    vector<double> result_operation;
+    result_operation = Method_Progonki(matrix, result);
+
+    size_t n = result_operation.size();
+
+    std::cout << "\nResult: " << std::endl;
+    for (int i = 0; i < n; i++) {
+        cout.precision(6);
+        cout << fixed << result_operation[i] << " ";
+    }
+
+    double det = determinant(matrix, matrix.size());
+    std::cout << "\n\nDeterminant: " << std::endl;
+
+    cout.precision(6);
+    cout << det << endl;
+
+    return 0;
+}
