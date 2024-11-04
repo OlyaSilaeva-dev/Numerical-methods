0d4607729aa6c7449539a87f00e4291cec8ac036
OlyaSilaeva-dev <olya-dom2014@yandex.ru>
2024-11-04 16:11:34 +0300

    Rotation_method done

--- lab4-Rotation_method/Rotation_method.h
+++ lab4-Rotation_method/Rotation_method.h
@@ -11,7 +11,7 @@ const double EPS = 1e-9;
 matrix transpose (matrix &other) {
     int n = other.get_lines_cnt();
     int m = other.get_columns_cnt();
-    vector<vector<double>> transposed_matrix(m, vector<double> (n, 0));
+    vector<vector<double>> transposed_matrix(m, vector<double> (n, 0.0));
 
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
@@ -24,6 +24,7 @@ matrix transpose (matrix &other) {
 }
 
 void input_matrix(const string &file_name, matrix &input_matrix) {
+
     ifstream fin(file_name);
     if (!fin.is_open()) {
         throw std::runtime_error("Wrong input file: " + file_name + "\n");
@@ -36,35 +37,35 @@ void input_matrix(const string &file_name, matrix &input_matrix) {
 
 double sum_non_diagonal_elements (matrix &_matrix) {
     int n = _matrix.get_lines_cnt();
-    double sum = 0;
+    double sum = 0.0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             if (i != j) {
-                sum += _matrix[i][j];
+                sum += abs(_matrix[i][j]);
             }
         }
     }
     return sum;
 }
 
-void Rotation_method(const matrix &_matrix, vector<double> &eigenvalues, vector<vector<double>> &eigenvectors) {
+void Rotation_method(const matrix &_matrix, vector<double> &eigenvalues, matrix &eigenvectors) {
     matrix A = _matrix;
     matrix A_next;
 
     if (A.get_lines_cnt() != A.get_columns_cnt()) {
-        throw runtime_error("");
+        throw runtime_error("The matrix is not a square!");
     }
 
     vector<matrix> U;
     int n = A.get_lines_cnt();
 
     do {
-        double max_element = EPS;
+        double max_element = 0.0;
         int max_i, max_j;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i != j && A[i][j] > max_element) {
+                if (i != j && abs(A[i][j]) > max_element) {
                     max_element = A[i][j];
                     max_i = i;
                     max_j = j;
@@ -76,22 +77,22 @@ void Rotation_method(const matrix &_matrix, vector<double> &eigenvalues, vector<
         if (abs(delta) < EPS) {
             throw runtime_error("division by zero!!!");
         }
-        auto fi = 1.0 / 2.0 * atan(2 * max_element / delta);
+        auto fi = 0.5 * atan2(2 * max_element, delta);
 
-        vector<vector<double>> u_vec(n, vector<double>(n, 0));
+        vector<vector<double>> u_vec(n, vector<double>(n, 0.0));
         matrix Uk(u_vec);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if ((i == max_i && j == max_i) && (i == max_j && j == max_j)) {
+                if ((i == max_i && j == max_i) || (i == max_j && j == max_j)) {
                     Uk[i][j] = cos(fi);
                 } else if (i == max_i && j == max_j) {
                     Uk[i][j] = -sin(fi);
                 } else if (i == max_j && j == max_i) {
                     Uk[i][j] = sin(fi);
                 } else if (i == j) {
-                    Uk[i][j] = 1;
+                    Uk[i][j] = 1.0;
                 } else {
-                    Uk[i][j] = 0;
+                    Uk[i][j] = 0.0;
                 }
             }
         }
@@ -99,7 +100,12 @@ void Rotation_method(const matrix &_matrix, vector<double> &eigenvalues, vector<
         U.push_back(Uk);
 
         matrix TU = transpose(Uk);
+
         A_next = TU * A * Uk;
+        if (A_next == A) {
+            break;
+        }
+
         A = A_next;
     } while (abs(sum_non_diagonal_elements(A)) > EPS);
 
@@ -114,7 +120,7 @@ void Rotation_method(const matrix &_matrix, vector<double> &eigenvalues, vector<
     for (int i = 1; i < U.size(); i++) {
         compos = compos * U[i];
     }
-    eigenvectors = transpose(compos).get_matrix();
+    eigenvectors = transpose(compos);
 }
 
 #endif //CM_ROTATION_METHOD_H
--- lab4-Rotation_method/test.cpp
+++ lab4-Rotation_method/test.cpp
@@ -1,5 +1,26 @@
 #include "Rotation_method.h"
+#include <filesystem>
 
 int main () {
+    matrix _matrix;
+    filesystem::current_path(R"(C:\src\GitHub\Numerical-methods\lab4-Rotation_method)");
+    input_matrix("test.txt", _matrix);
+
+    std::cout << "Matrix: \n"<< _matrix << std::endl;
+
+    vector<double> eigenvalues;
+    matrix eigenvectors;
+    Rotation_method(_matrix, eigenvalues, eigenvectors);
+
+    for (int i = 0; i < eigenvalues.size(); i++) {
+        std::cout << "Value " << i << ": " << eigenvalues[i] << "\t";
+        std::cout << "Vector: ";
+        for (auto v : eigenvectors.get_matrix()[i]) {
+            std::cout << v << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+
 }
 
